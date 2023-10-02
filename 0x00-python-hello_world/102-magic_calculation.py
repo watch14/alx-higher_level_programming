c9dcf858529b9f145e7ce6bab75d0ffa9c3bc92e
watch14 <watchman1414@gmail.com>
2023-10-02 14:37:21 +0000

    fast_push

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-print("""  3           0 LOAD_CONST               1 (98)
-              3 LOAD_FAST                0 (a)
-              6 LOAD_FAST                1 (b)
-              9 BINARY_POWER
-             10 BINARY_ADD
-             11 RETURN_VALUE""")
+def magic_calculation(a, b):
+    return 98 + a ** b
