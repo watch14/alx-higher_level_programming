d2025b18a07d0acd7a938aefe9c21ebf70e04a42
watch14 <watchman1414@gmail.com>
2023-10-03 11:19:38 +0000

    fast_push

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+x = abs(number) % 10
+if x > 5:
+    m = "and is greater than 5"
+elif x == 0:
+    m = "and is 0"
+else:
+    m = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {x:d} and {m}")
