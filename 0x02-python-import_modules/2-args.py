aaed1c179870d2827f18fd079a8e7b6439e2ada8
watch14 <watchman1414@gmail.com>
2023-10-05 15:22:13 +0000

    fast_push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ import sys
 if __name__ == "__main__":
     num = len(sys.argv) - 1
     if num == 0:
-        print("0 arguments:")
+        print("0 arguments.")
+    elif num == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(num))
     for i in range(1, num + 1):
