12f7234f418e29915a003f084cdf3f2bc5e9b408
watch14 <watchman1414@gmail.com>
2023-10-05 14:16:05 +0000

    fast_push

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-x = add(1, 2)
-print(f"{a} + {b} = {x}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
