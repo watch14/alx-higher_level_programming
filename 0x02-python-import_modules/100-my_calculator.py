8112992107968a247ee427cff313221ac678b3f4
watch14 <watchman1414@gmail.com>
2023-10-05 16:03:48 +0000

    fast_push

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
@@ -19,7 +19,6 @@ if __name__ == "__main__":
         print("{} * {} = {}".format(a, b, mul(a, b)))
     elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
-    
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
