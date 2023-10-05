1196fc81213a100748a0ff676f1363cdacc733e4
watch14 <watchman1414@gmail.com>
2023-10-05 15:00:22 +0000

    fast_push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     l = len(sys.argv) - 1
     if l == 0:
         print("0 arguments.")
+    if l == 1:
+        print("1 arguments.")
     else:
         print("{} arguments:".format(l))
     for i in range(l):
