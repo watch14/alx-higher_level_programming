62debfd7dabd80032e759a5b9533155fd7b0a5c0
watch14 <watchman1414@gmail.com>
2023-10-05 15:36:07 +0000

    fast_push

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv)
+    lum = len(sys.argv)
     x = 0
-    for i in range(1,l):
+    for i in range(1, lum):
         x = x + int(sys.argv[i])
     print(x)
