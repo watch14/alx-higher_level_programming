2b8baa87adef060ccde6fa6ba9d4f6871bd9a179
watch14 <watchman1414@gmail.com>
2023-10-08 12:49:47 +0000

    fast_push

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-
+    t1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    t2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    p1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    p2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (t1 + t2, p1 + p2)
