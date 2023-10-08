ffca2692c8e13771a8fd43c7bca7b2c5a86c9189
watch14 <watchman1414@gmail.com>
2023-10-08 12:25:09 +0000

    fast_push

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     new = my_list[:]
-    if idx < 0 or idx >= len(new):
-        return new_in_list
-    else:
-        new[idx] = element
-        return new
+    new[idx] = element
+    return new
