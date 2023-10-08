34c0bc24856d0f25ab4a0c76cb62f7cb175c38f8
watch14 <watchman1414@gmail.com>
2023-10-08 13:35:32 +0000

    fast_push

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new = [my_list[i] for i in range(len(my_list)) if i != idx]
-    return new
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
