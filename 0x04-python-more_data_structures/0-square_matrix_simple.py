4cdf3288d6d5dc5014506fa8ef20655f4fec4cef
watch14 <watchman1414@gmail.com>
2023-10-11 17:04:48 +0000

    fast_push

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [[element**2 for element in row] for row in matrix]
+    new = [[i ** 2 for i in row] for row in matrix]
     return new
