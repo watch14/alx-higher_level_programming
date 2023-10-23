e56d4aaacb81716bed9fcda066784285f1758dcd
watch14 <watchman1414@gmail.com>
2023-10-23 09:25:12 +0000

    fast_push

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as er:
-        print("Exception: ", er, file=sys.stderr)
+        print("Exception:", er, file = sys.stderr)
         r = False
     return r
