c764d2eece1b3ea913da354f8cc43e3d5bd55feb
watch14 <watchman1414@gmail.com>
2023-10-24 10:52:04 +0000

    fast_push

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""SQUARES"""
+
+
+class Square:
+    """Squares"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        x = self.size
+        for i in range(x):
+            for j in range(x):
+                print("#", end="")
+            print()
