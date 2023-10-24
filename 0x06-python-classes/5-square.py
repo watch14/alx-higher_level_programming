389f82a591bfa81a53bcbed138724fa9b8638829
watch14 <watchman1414@gmail.com>
2023-10-24 11:01:06 +0000

    fast_push

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -24,8 +24,6 @@ class Square:
 
     def my_print(self):
         x = self.size
-        if x == 0:
-            return
         for i in range(x):
             for j in range(x):
                 print("#", end="")
