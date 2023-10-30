bf7dcb72b9e93f0204e627fae91de5e17e4396fc
watch14 <watchman1414@gmail.com>
2023-10-30 13:15:08 +0000

    fast_push

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,32 +5,29 @@
 class Rectangle:
     """RECTANGLE CLASS"""
     def __init__(self, width=0, height=0):
-        """Rectangle"""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Rectangle"""
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
         @property
     def height(self):
-        """Rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Rectangle"""
         if not isinstance(height, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__height = value
