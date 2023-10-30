3bdfc35446dd118c3e01c793800e377d362db887
watch14 <watchman1414@gmail.com>
2023-10-30 13:41:29 +0000

    fast_push

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -36,4 +36,6 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
         return 2 * (self.width + self.height)
