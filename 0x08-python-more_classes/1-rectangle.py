73b61e0ba2f09158a9679f885565d165db432180
watch14 <watchman1414@gmail.com>
2023-10-30 13:17:12 +0000

    fast_push

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-        @property
+    @property
     def height(self):
         return self.__height
 
