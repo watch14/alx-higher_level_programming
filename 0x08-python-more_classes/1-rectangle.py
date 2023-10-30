1963d443cb52c8ddee3135322ffbffbbb731f489
watch14 <watchman1414@gmail.com>
2023-10-30 12:53:11 +0000

    fast_push

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+class Rectangle:
+    """RECTANGLE CLASS"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
