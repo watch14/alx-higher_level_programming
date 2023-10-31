9fe1159bff9c77162291db476e513b34728d2311
watch14 <watchman1414@gmail.com>
2023-10-31 14:17:35 +0000

    fast_push

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
-    return "BestSchool, " * magic_string.counter + "BestSchool"
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestShool" for i in range(magic_string.count)])
