9cd62cf1b1bf74856013158f9b63df9348e6b304
watch14 <watchman1414@gmail.com>
2023-10-31 14:14:01 +0000

    fast_push

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, 'count'):
-        magic_string.count = 0
-    else:
-        magic_string.count += 1
-    return "BestSchool" + ", BestSchool" * magic_string.count
+        magic_string.count 0
+    return "BestSchool" + ", BestSchool" * magic_string.count =+1
