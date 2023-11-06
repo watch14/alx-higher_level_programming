43cbca224c6827d68818c92f666aadfbd12f6799
watch14 <watchman1414@gmail.com>
2023-11-06 21:42:45 +0000

    fast_push

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Square inherits Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+
+    def __init__(self, size):
+        """init"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area():
+        """area"""
+        return super().area()
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
