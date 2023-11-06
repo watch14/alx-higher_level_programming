fedb2e3ae9f50c6b0b423ab6b4ac7fa49a381f24
watch14 <watchman1414@gmail.com>
2023-11-06 21:17:24 +0000

    fast_push

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,4 +15,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
