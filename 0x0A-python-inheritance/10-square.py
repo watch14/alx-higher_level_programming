d7e264789a3abd5ddd4dc61911465033e5462280
watch14 <watchman1414@gmail.com>
2023-11-06 21:31:05 +0000

    fast_push

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Square inherits Rectangle"""
+"""
+Square inherits Rectangle
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Square"""
+    
     def __init__(self, size):
         super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
 
     def area():
-        return self.__width * self.__height
+        return self.__size ** 2
