ff2da4a8ba6a2750ae3a16582f8f1fe93c6f9116
watch14 <watchman1414@gmail.com>
2023-11-06 21:31:30 +0000

    fast_push

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,7 +7,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square"""
-    
+
     def __init__(self, size):
         super().__init__(size, size)
         self.integer_validator("size", size)
