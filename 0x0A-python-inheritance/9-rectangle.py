a2e9291fa2a0a4e13745111ff8ed50f5019e8341
watch14 <watchman1414@gmail.com>
2023-11-06 21:48:02 +0000

    fast_push

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,6 +2,7 @@
 """Rectangle inheret geo"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle"""
     def __init__(self, width, height):
@@ -18,4 +19,3 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """String of rectangle"""
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
