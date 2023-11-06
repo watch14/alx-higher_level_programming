092340b629dc719d4df0d6704ea1d89bfea44e90
watch14 <watchman1414@gmail.com>
2023-11-06 21:09:15 +0000

    fast_push

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Rectangle class that inherits from BaseGeometry."""
-BaseGeometry = ___import___('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Rectangle"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
