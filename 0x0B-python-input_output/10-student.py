ceb03dc461372b6ff9db5705bd38cd1b69e99caf
watch14 <watchman1414@gmail.com>
2023-11-07 20:58:14 +0000

    fast_push

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""class student"""
-
+"""Class Student: Defines a Student class"""
 
 class Student:
-    """student info"""
+    """Student info"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """to json"""
-        try:
-            for at in attrs:
-                if type(at) in not str:
-                    return self.__dict__
-        except Exception:
+        """to JSON"""
+        if attrs is None:
             return self.__dict__
-
-        info = dict()
+        
+        info = {}
         for key, value in self.__dict__.items():
             if key in attrs:
                 info[key] = value
