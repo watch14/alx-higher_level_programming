be8a63d472c538b1c9085fcf86384e03e4a64980
watch14 <watchman1414@gmail.com>
2023-11-07 20:13:18 +0000

    fast_push

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """save file .json"""
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, file)
