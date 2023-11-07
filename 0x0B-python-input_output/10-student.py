0ba83c00bead2a66e1fb1d46b31c0400f03d5544
watch14 <watchman1414@gmail.com>
2023-11-07 20:59:07 +0000

    fast_push

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class Student: Defines a Student class"""
 
+
 class Student:
     """Student info"""
     def __init__(self, first_name, last_name, age):
@@ -12,7 +13,7 @@ class Student:
         """to JSON"""
         if attrs is None:
             return self.__dict__
-        
+
         info = {}
         for key, value in self.__dict__.items():
             if key in attrs:
