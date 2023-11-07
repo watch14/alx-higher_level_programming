874b2af66407a7985b30936ed31efb89d30f1c01
watch14 <watchman1414@gmail.com>
2023-11-07 20:29:06 +0000

    fast_push

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""add item"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+args = lis(sys.argv[1:])
+
+try:
+    data = load_from_json_file(add_item.json)
+except Exception:
+    data = []
+
+data.extend(args)
+save_to_json_file(data, 'add_item.json')
