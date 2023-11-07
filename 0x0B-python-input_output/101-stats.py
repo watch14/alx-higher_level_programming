8c3cdf8e31e053aae7d106aa0f4d7cfa4d4fa84d
watch14 <watchman1414@gmail.com>
2023-11-07 21:40:07 +0000

    fast_push

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
-"""stats"""
+"""stdin"""
+
+
 from sys import stdin
 
 
-def print_stats(size, cnt):
+def print_stats(file_size, status_counts):
     """stdin"""
-    print("File size: {}".format(size))
-    for status_code in sorted(cnt.keys()):
-        if cnt[status_code] > 0:
-            print("{}: {}".format(status_code, cnt[status_code]))
+    print("File size: {}".format(file_size))
+    for status_code in sorted(status_counts.keys()):
+        if status_counts[status_code] > 0:
+            print("{}: {}".format(status_code, status_counts[status_code]))
+
 
 def main():
     try:
-        line = 0
-        size = 0
-        cnt = {
-            200: 0,
-            301: 0,
-            400: 0,
-            401: 0,
-            403: 0,
-            404: 0,
-            405: 0,
-            500: 0,
-        }
-
-        for data in sys.stdin:
-            line += 1
-            parts = data.split()
+        line_number = 0
+        total_file_size = 0
+        status_counts = {
+                200: 0,
+                301: 0,
+                400: 0,
+                401: 0,
+                403: 0,
+                404: 0,
+                405: 0,
+                500: 0,
+                }
+        for line in sys.stdin:
+            line_number += 1
+            parts = line.split()
             if len(parts) >= 7:
                 status_code = int(parts[-2])
                 file_size = int(parts[-1])
-                size += file_size
-                if status_code in cnt:
-                    cnt[status_code] += 1
-            if line % 10 == 0:
-                print_stats(size, cnt)
-    except KeyboardInterrupt:
-        print_stats(size, cnt)
+                total_file_size += file_size
+                if status_code in status_counts:
+                    status_counts[status_code] += 1
 
+            if line_number % 10 == 0:
+                print_stats(total_file_size, status_counts)
+    except KeyboardInterrupt:
+        print_stats(total_file_size, status_counts)
