cd2202b8c5d8625ddc7264d466bfaa50020d4fc1
watch14 <watchman1414@gmail.com>
2023-11-07 19:52:10 +0000

    fast_push

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write file"""
+
+
+def write_file(filename="", text=""):
+    """write text in file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
