eb7455ab79dc0668573fd7b08bfdcdca24fb6024
watch14 <watchman1414@gmail.com>
2023-11-12 09:59:00 +0000

    fast_push

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,8 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """dict"""
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,35 @@ class Square(Rectangle):
         """constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
-        '''Returns square.'''
+        """Returns square."""
         return '[{}] ({}) {}/{} - {}'.format(
-                type(self).__name__, self.id, self.x, self.y, self.width)
+                type(self).__name__, self.id, self.x, self.y, self.size)
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """updates attr"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """keyword args."""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
