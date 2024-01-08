afea06352a984b5d8f2894416d58254c70b85a73
watch14 <watchman1414@gmail.com>
2024-01-08 21:31:13 +0000

    fast_push

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-const x = process.argv[];
+const x = process.argv;
 
-
+if (x.length -2 == 0 || x.length -2 == 1) {
+  console.log(0);
+}
+for (let i = 0; i < x.length -2; i++) {
+}
