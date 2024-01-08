0a98c75784389d8dac110a8e378c3a00d7cdaab4
watch14 <watchman1414@gmail.com>
2024-01-08 19:26:33 +0000

    fast_push

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const x = process.argv.length - 2;
 
 if (x === 0) {
   console.log("No argument");
+} else if (x === 1) {
+  console.log("Argument found");
 } else {
   console.log("Arguments found");
 }
