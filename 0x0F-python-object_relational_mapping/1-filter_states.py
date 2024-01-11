449eeca93be87585ed5d5dc5f07964b403d8671a
watch14 <watchman1414@gmail.com>
2024-01-11 23:45:37 +0000

    fast_push

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host='localhost',
+            username=sys.args[1],
+            password=sys.args[2],
+            db=sys.argv[3],
+            port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM 'states' WHERE name LIKE 'n%' BY id ASC")
+    rows = cur.fetchall()
+    
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
