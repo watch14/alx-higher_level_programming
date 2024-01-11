3ce0a2b15fa614520a8827699fb73f7ecc7dc487
watch14 <watchman1414@gmail.com>
2024-01-11 23:23:10 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
+import sys
 import MySQLdb
 
-db = MySQLdb.connect(host='localhost', 
-                    username=sys.args[1], 
-                    password=sys.args[2], 
-                    db=sys.argv[3], 
-                    port = 3306)
+db = MySQLdb.connect(
+        host='localhost',
+        username=sys.args[1],
+        password=sys.args[2],
+        db=sys.argv[3],
+        port=3306)
 
 cur = db.cursor()
 cur.execute("SELECT * FROM 'states' BY id ASC")
 
 rows = cur.fetchall()
 
-from row in rows:
+for row in rows:
     print(row)
 
 cur.close()
