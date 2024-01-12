cd5a68851c8024ccddd6c4dcf48c7f3aa72ae780
watch14 <watchman1414@gmail.com>
2024-01-12 17:34:12 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
             port=3306
             )
 
-    cursor = db.cursor()
-
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
             JOIN states ON cities.state_id = states.id \
             ORDER BY cities.id ASC")
 
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
 
-    cursor.close()
+    cur.close()
+
     db.close()
