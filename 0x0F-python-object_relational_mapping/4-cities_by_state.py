53a84b6494a1d3229f9140a8a9faaea1075ad2a7
watch14 <watchman1414@gmail.com>
2024-01-12 17:31:24 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-""" start with N """
+"""Script to list cities from a MySQL database"""
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    """ ss """
+
+if __name__ == "__main__":
+    """Main execution block"""
     db = MySQLdb.connect(
-            host='localhost',
-            user=sys.argv[1],
-            password=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-            )
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
 
-    cur = db.cursor()
+    cursor = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-            JOIN states ON cities.state_id = states.id \
-            ORDER BY cities.id ASC")
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    ORDER BY cities.id ASC")
 
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
 
     for row in rows:
         print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
