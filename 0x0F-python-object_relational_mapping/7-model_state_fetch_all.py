bceded25526d081c8b5711e08f2d35248143749e
watch14 <watchman1414@gmail.com>
2024-01-12 23:40:57 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 """ALCHEMY"""
+
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}"
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for row from states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
