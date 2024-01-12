692d97271c0e5726eeb7c04075e366ce213f7dd7
watch14 <watchman1414@gmail.com>
2024-01-12 00:24:09 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-""" import form table """
+""" start with N """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
 
     db = MySQLdb.connect(
+            host='localhost',
             user=sys.argv[1],
             password=sys.argv[2],
             db=sys.argv[3],
-            host='localhost',
-            port=3306)
+            port=3306
+            )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states \
+            WHERE name = '{}' \
+            ORDER BY id ASC".format(sys.argv[4]))
 
     rows = cur.fetchall()
-
+    
     for row in rows:
         print(row)
 
