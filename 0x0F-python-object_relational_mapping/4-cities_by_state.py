0794064e7da811c8f9c6caf929da1c3a734085c2
watch14 <watchman1414@gmail.com>
2024-01-12 17:31:55 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,18 +7,18 @@ import MySQLdb
 if __name__ == "__main__":
     """Main execution block"""
     db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        host="localhost",
-        port=3306
-    )
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
 
     cursor = db.cursor()
 
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                    JOIN states ON cities.state_id = states.id \
-                    ORDER BY cities.id ASC")
+            JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC")
 
     rows = cursor.fetchall()
 
