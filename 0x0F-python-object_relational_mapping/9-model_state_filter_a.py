321cc5bc296e92a6f7d102d194d466c408072d25
watch14 <watchman1414@gmail.com>
2024-01-13 00:34:34 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,19 +7,18 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost:3306/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
+            f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]} \
+                    @localhost:3306/{sys.argv[3]}")
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
     states = (
             session.query(State)
             .filter(State.name.like('%a%'))
             .order_by(State.id)
-            .all())
+            .all()
+            )
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
