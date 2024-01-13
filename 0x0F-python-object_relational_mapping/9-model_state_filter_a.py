f04dd57b7c59593283f7d1aa7163ad39c69d2043
watch14 <watchman1414@gmail.com>
2024-01-13 00:29:22 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine(
             f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]\
-                    @localhost:3306/{sys.argv[3]}}")
+                    @localhost:3306/{sys.argv[3]}}", pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
 
