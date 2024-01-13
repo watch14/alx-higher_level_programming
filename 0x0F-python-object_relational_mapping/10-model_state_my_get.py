c20e7147a635163d231d62f4d6d9a1f375f5a526
watch14 <watchman1414@gmail.com>
2024-01-13 00:43:20 +0000

    fast_push

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""SQLAlch"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+            f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]} \
+                    @localhost:3306/{sys.argv[3]}")
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = (
+            session.query(State)
+            .filter(State.name.like("sys.argv[4]"))
+            .order_by(State.id)
+            .all()
+            )
+
+    if state:
+        print(f"{state.id}")
+    else:
+        print("Not found")
+
+    session.close()
