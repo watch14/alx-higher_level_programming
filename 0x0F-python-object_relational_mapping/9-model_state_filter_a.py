f5199522cd4a3c06f78f4d318a90741cda4723d3
watch14 <watchman1414@gmail.com>
2024-01-13 00:22:30 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
             session.query(State)
             .filter(State.name.like('%a'))
             .order_by(State.id)
+            .all()
             )
 
     for state in states:
