1e7f214a220feb8dfdea200e96eac4d0bfb201e9
watch14 <watchman1414@gmail.com>
2024-01-14 01:32:42 +0000

    fast_push

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" m """
+"""Module to define the City class"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
-class State(Base):
-    """MySQL table"""
+class City(Base):
+    """City class definition"""
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
     name = Column(String(128), nullable=False)
+
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
