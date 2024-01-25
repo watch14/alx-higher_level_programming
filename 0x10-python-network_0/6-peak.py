be6d15ac8991e9dec28aed7091848fcaa848e88d
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-25 10:19:38 +0000

    fast_push

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,5 +3,18 @@
 
 
 def find_peak(list_of_integers):
-    sorted_list = sorted(list_of_integers)
-    return sorted_list[-1]
+    """ find peak """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
