fdd89dbcccff3761c65f8037a241a1a8fd111834
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-26 10:21:33 +0000

    fast_push

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Fetches arg.1"""
-import urllib.request
+"""Fetche"""
+import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as res:
-        cont = res.read().decode("utf-8")
-        print(f"Body response:")
-        print(f"\t- type: {type(cont)}")
-        print(f"\t- content: {cont}")
+    res = requests.get(url)
+    print(f"Body response:")
+    print(f"\t- type: {type(res.text)}")
+    print(f"\t- content: {res.text}")
