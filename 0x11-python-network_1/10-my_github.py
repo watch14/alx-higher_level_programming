cfbb1723afbec92c49709e413eaa7672c33055ce
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-26 11:28:06 +0000

    fast_push

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     url = "https://github.com/user"
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get(url, auth=auth)
-    data = r.json()
-    if data:
-        print(data["id"])
-    else:
-        print('None')
+
+    try:
+        data = r.json()
+        if data:
+            print(data["id"])
+    except:
+        print("None")
