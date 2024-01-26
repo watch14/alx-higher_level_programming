eaa7b868b2ef05d4ea455cf864f7f3e3b7a47fde
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-26 11:51:20 +0000

    fast_push

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,8 +4,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user_repo = sys.argv[2]"/"sys.argv[1]
-    url = f"https://developer.github.com/{rep}/commits/"
+    user_repo = f"sys.argv[2]/sys.argv[1]"
+    url = f"https://developer.github.com/{user_repo}/commits/"
 
     req = resuests.get(url)
     data = req.json()
