0605f7a5ce2b0abf7c40ed25c98f9d293c5f974b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-26 11:38:07 +0000

    fast_push

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://api.github.com/user"
-    auth = (argv[1], argv[2])
-    request = requests.get(url, auth=auth)
+    au = (argv[1], argv[2])
+    request = requests.get(url, auth=au)
     print(request.json().get("id"))
