586920d13560a00804dc52f01ba1628f00ec4a50
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-26 11:36:24 +0000

    fast_push

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-""" REQUESTS - github API """
+""" """
+
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://github.com/user"
-    auth = (sys.argv[1], sys.argv[2])
-
-    try:
-        res = requests.get(url, auth=auth)
-        res.raise_for_status()
-        data = res.json()
-
-        print(data["id"])
-
-    except requests.exceptions.RequestException:
-        print("None")
+    auth = (argv[1], argv[2])
+    request = requests.get("https://api.github.com/user", auth=auth)
+    print(request.json().get("id"))
