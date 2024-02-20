0afb757f86677dacc38459cdc28859bc0479083b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-20 10:28:25 +0100

    fast_push

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+request.get(url, (err, resp, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(file, body, 'utf-8', err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
