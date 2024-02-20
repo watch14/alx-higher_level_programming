f76f2ed63e1ed96fc773bd48b537efe26c817c66
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-20 09:04:08 +0100

    fast_push

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
