80cf2e779add20080ff702b880e3867519b2eb40
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-20 10:17:10 +0100

    fast_push

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], (error, response, body) {
+request(process.argv[2], (error, response, body) => {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
