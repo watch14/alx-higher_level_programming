3f853b8a9e1c5b17fe4e3f16bd338c379c4f487d
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-20 09:26:17 +0100

    fast_push

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], (err, response) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
